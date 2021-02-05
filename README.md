1d756bd93894d6b79e0b68966403aff63c3ddbc4
Nikhil Mahajan <nikhilm92@gmail.com>
2021-02-05 01:51:17 -0500

    Add another tech.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GigaResearch
 
-A Factorio mod.
+A Factorio mod that adds "billion science" infinite technologies for
+testing purposes - useful for testing large megabase designs for long
+periods of time.
--- giga.lua
+++ giga.lua
@@ -1,26 +1,48 @@
-data:extend(
-    {
-        {
-            type = "technology",
-            name = "giga-research",
-            icon = "__GigaResearch__/graphics/giga.png",
-            icon_size = 256,
-            prerequisites = {"space-science-pack"},
-            unit = {
-                count = 1000000000,
-                ingredients = {
-                    {"automation-science-pack", 1},
-                    {"logistic-science-pack", 1},
-                    {"chemical-science-pack", 1},
-                    {"production-science-pack", 1},
-                    {"utility-science-pack", 1},
-                    {"space-science-pack", 1},
-                },
-                time = 60
-            },
-            max_level = "infinite",
-            upgrade = true,
-            order = "zz-gr-a"
-        }
-    }
-)
+local giga_all = {
+    type = "technology",
+    name = "giga-research-all",
+    icon = "__GigaResearch__/graphics/giga-all.png",
+    icon_size = 256,
+    prerequisites = {"space-science-pack"},
+    unit = {
+        count = 1000000000,
+        ingredients = {
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1},
+            {"chemical-science-pack", 1},
+            {"military-science-pack", 1},
+            {"production-science-pack", 1},
+            {"utility-science-pack", 1},
+            {"space-science-pack", 1},
+        },
+        time = 30
+    },
+    max_level = "infinite",
+    upgrade = true,
+    order = "zz[giga-research-all]"
+}
+
+local giga_nomil = {
+    type = "technology",
+    name = "giga-research-nomil",
+    icon = "__GigaResearch__/graphics/giga-nomil.png",
+    icon_size = 256,
+    prerequisites = {"space-science-pack"},
+    unit = {
+        count = 1000000000,
+        ingredients = {
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1},
+            {"chemical-science-pack", 1},
+            {"production-science-pack", 1},
+            {"utility-science-pack", 1},
+            {"space-science-pack", 1},
+        },
+        time = 60
+    },
+    max_level = "infinite",
+    upgrade = true,
+    order = "zz[giga-research-nomil]"
+}
+
+data:extend{giga_nomil, giga_all}
